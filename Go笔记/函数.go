e762383e89d66b622cdeb024ff1a60506fc25bd9
fke2018 <shuqian2018@gmail.com>
2020-06-08 09:50:20 +0800

    更新4.5 延迟调用

--- Go笔记/函数.go
+++ Go笔记/函数.go
@@ -21,7 +21,6 @@ code is far away from bugs with the god animal protecting
 package main
 
 
-
 // 4.1      关键字func用于定义函数
 //* 1. 无需前置声明
 //* 2. 不支持命名嵌套定义(nested)
@@ -379,4 +378,305 @@ package main
 
 
 
-// 匿名函数     没有定义名字符号的函数
+// 4.4 匿名函数     ①没有定义名字符号的函数；②匿名函数可以直接调用，保存到变量，作为参数或返回值； ③不曾使用的匿名函数会被编译器当作错误；
+//             ④相比语句块，匿名函数的作用域被隔离(不使用闭包)，更加灵活，不会引发外部污染；⑤没有定义顺序限制，必要时可以抽离
+//func main() {
+//    func (s string) {
+//        println(s)
+//    } ("hello world!")                    // 直接执行
+//}
+
+
+//func main() {
+//    add := func(x, y int) int {           // 赋值给变量
+//        return x + y
+//    }
+//
+//    println(add(1, 3))
+//}
+
+
+//func test(f func()) {
+//    f()
+//}
+//
+//func main() {
+//    test(func() {                         // 作为参数
+//        println("hello, world!")
+//    })
+//}
+
+
+//func test() func(int, int) int {
+//    return func(x int, y int) int {         // 作为返回值
+//        return x + y
+//    }
+//}
+//
+//func main() {
+//    add := test()
+//    println(add(2, 3))
+//}
+
+
+
+//func testStruct() {
+//    type calc struct {                              // 定义结构体类型
+//        mul func(x, y int) int                      // 函数类型字段
+//    }
+//
+//    x := calc{
+//        mul: func(x, y int) int {
+//            return x * y
+//        },
+//    }
+//
+//    println(x.mul(3, 4))
+//}
+//
+//func testChannel() {
+//    c := make(chan func(int, int) int, 2)
+//
+//    c <- func(x int, y int) int {
+//        return x + y
+//    }
+//
+//    println((<-c)(1, 2))
+//}
+//
+//func main() {
+//    a := testStruct
+//    b := testChannel
+//
+//    a()
+//    b()
+//}   // 普通函数和匿名函数都可以作为结构体字段，或经通道传递
+
+
+
+
+// 闭包       函数和其引用的环境组合体
+//func test(x int) func() {           // test返回的匿名函数会引用上下文环境变量x
+//    return func() {
+//        println(x)
+//    }
+//}
+//
+//func main() {
+//    f := test(123)              // test在main函数中执行时，它依然可正确读取x的值，这种现象就称作闭包
+//    f()
+//}
+
+
+
+//func test(x int) func() {
+//    println(&x)
+//
+//    return func() {                 // 如果分析汇编代码，其实可以发现返回的不仅仅是匿名函数，还有其引用的环境变量指针，所以说闭包是函数和其引用的环境组合体
+//        println(&x, x)
+//    }
+//}
+//
+//func main() {
+//    f := test(0x100)
+//    f()
+//}
+
+
+
+//func test() []func() {
+//    var s []func()
+//
+//    for i := 0; i < 3; i++ {                    // for循环复用局部变量i，添加操作只是将匿名函数放入列表，并未执行；所以main执行这些函数时，读取的是环境变量最后一次循环的值3
+//        s = append(s, func() {                  // 将多个匿名函数添加到列表
+//            println(&i, i)
+//        })
+//    }
+//
+//    return s                                    // 返回匿名函数列表
+//}
+//
+//func main() {
+//    for _, f := range test() {                  // 迭代执行所有的匿名函数
+//        f()
+//    }
+//}
+
+
+
+//func test() []func() {
+//    var s []func()
+//
+//    for i := 0; i < 3; i++ {
+//        x := i                              // x每次循环都重新定义了
+//        s = append(s, func() {
+//            println(&x, x)
+//        })
+//    }
+//
+//    return s
+//}
+//
+//func main() {
+//    for _, f := range test() {
+//        f()
+//    }
+//}
+
+
+
+//func test(x int) (func(), func()) {                 //返回2个匿名函数
+//    return func() {
+//        println(x)
+//        x += 10                                     // 修改环境变量
+//    }, func() {
+//        println(x)                                  // 打印环境变量
+//    }
+//}
+//
+//func main() {
+//    a, b := test(100)
+//    b()
+//    a()
+//    b()
+//}   // 多个匿名函数引用同一个环境变量，任何修改行为都会影响到其他函数取值，在并发模式下需要做同步处理
+
+
+
+
+
+
+
+
+//*********************************************************************************************
+
+
+
+
+
+
+
+
+// 4.5 延迟调用     语句defer向当前函数注册后执行的函数调用，被称为延迟调用，因为它们直到当前函数执行结束前才被执行，常用于资源释放，以及错误处理等操作
+//import (
+//    "log"
+//    "os"
+//)
+//
+//func main() {
+//    f, err := os.Open("表达式.go")
+//    if err != nil {
+//        log.Fatalln(err)
+//    }
+//
+//    defer  f.Close()                // 仅注册，直到main退出前才执行
+//}  //  伪代码
+
+
+
+//func main() {
+//    x, y := 1, 2
+//
+//    defer func(a int) {
+//        println("defer x, y = ", a, y)     // y为闭包引用
+//    } (x)                                  // 注册时复制调用函数
+//
+//    x += 100                               // 对x的修改不会影响延迟调用
+//    y += 200
+//    println(x, y)
+//}   // 延迟调用注册的是调用，必须提供执行所需的参数(哪怕为空）。参数值在注册时被复制并缓存起来。如对状态敏感可以改用指针或者闭包
+
+
+
+//func main() {
+//    defer println("a")
+//    defer println("b")
+//}   // 输出结果b a;     多个延迟注册按FILO次序执行
+
+
+
+//func test() (z int) {
+//    defer func() {
+//        println("defer:", z)
+//        println("2:", z)
+//        z += 100                            // 修改命名返回值
+//        println("3:", z)
+//    }()
+//
+//    println("1:", z)
+//    return 100                          // 实际执行次序 z = 100, call defer, ret
+//}
+//
+//func main() {
+//    println("test:", test())
+//}   // return和panic语句会终止当前函数流程，引发延迟调用。另外return语句，它会先更新返回值
+
+
+
+
+// 误用   重点：延迟调用是在函数结束时才被执行。不合理的使用方式会浪费很多资源，甚至造成逻辑错误
+//eg:    循环处理多个日志文件，不恰当的defer导致文件关闭时间延长
+//import (
+//    "fmt"
+//    "log"
+//    "os"
+//)
+//
+//func main() {
+//    for i := 0; i < 10; i++ {
+//        path := fmt.Sprintf("./log/%d.txt", i)
+//
+//        f ,err := os.Open(path)
+//        if err != nil {
+//            log.Println(err)
+//            continue
+//        } else {
+//            fmt.Printf("f detail : %v\n", f)
+//        }
+//
+//     // 这个关闭操作在main函数结束时才会执行，而不是当前循环中执行
+//     // 这无端延长了逻辑结束时间和f的生命周期，平白多消耗量内存等资源
+//     defer f.Close()
+//    }
+//}
+
+
+// 上面eg例子 应该直接调用，或者重构为函数，将循环个处理算法分离
+//import (
+//    "fmt"
+//    "os"
+//    "log"
+//)
+//
+//func main() {
+//    // 日志处理算法
+//    do := func(n int) {
+//        path := fmt.Sprintf("./log/%d.txt", n)
+//        f ,err := os.Open(path)
+//        if err != nil {
+//            log.Println(err)
+//        }
+//        // 该延迟调用在此匿名函数结束时才执行，而非main
+//        defer f.Close()
+//    }
+//    for i := 0; i < 10; i++ {
+//        do(i)
+//    }
+//}
+
+
+
+
+
+
+
+//*********************************************************************************************
+
+
+
+
+
+
+
+
+
